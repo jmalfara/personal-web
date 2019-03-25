0ea601be0e4e51cb419e3ced2054a401b3b73bfa
jmalfara <jordan.malfara@gmail.com>
2019-03-25 19:29:36 -0400

    Create folder structure and really basic index page

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Personal Website
+
+This project is a proving ground for me to learn web and try out cool stuff
---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Toggle the expand collapse of the top navigation bar
+function toggleExpand() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
